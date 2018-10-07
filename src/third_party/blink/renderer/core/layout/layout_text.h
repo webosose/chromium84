ceb483838af904832e3aeaf007080924b98e75db
nilesh.patil <nilesh.patil@lge.com>
2018-10-08 00:20:55 +0900

    [op][upsable][a11y] Audio guidance is not heard for first character
    :Release Notes:
    Audio readout does not happen when first character is entered in
    input box
    
    :Detailed Notes:
    No audio is heard when user inputs first character in input box as
    no value change event is generated from Webkit.
    It looks like Inline Text box is created lazily for Text Control.
    However as Web Area or root web area is created. Hence we receive
    textSelectionChange event which is generated for web area.
    ValueChange event is missed for first character.
    However, for subsequent character inputs valueChanged/textChanged is
    generated because InlineTextBox is already present.
    Hence audio guidance is heard.
    Same is true when user starts deleting characters.
    
    So, now we update the text if necessary, when LayoutText is created.
    
    :Testing Performed:
    1. Launch Enyo2Sampler
    2. Goto Moonstone->Input
    3. Select 'Just Type' input field
    4. Type any single alphabet.
    5. Check audio guidance is heard for typed alphabet.
    
    :QA Notes:
    Test-cases: GT-23528
    
    :Issues Addressed:
    [PLAT-40947] Accessibility: Do not read the first input in
                 the input field.
    [PLAT-117504] Port : [op][upsable][a11y] Audio guidance is not heard for first character
    
    Change-Id: Ifc644fc753f7c760d08d6620230d21caa875e5ce
    Originally-Reviewed-on: http://gpro.lge.com/c/webos-pro/chromium79/+/267111
    Reviewed-on: http://gpro.lge.com/c/webos-pro/chromium84/+/293734
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Jose Dapena <jose.dapena@lge.com>
    Reviewed-by: Donghyun Kim <donghyun11.kim@lge.com>
    Tested-by: Jose Dapena <jose.dapena@lge.com>
    Tested-by: Test BTS <test.bts@lge.com>
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium84/+/296079
    Tested-by: Donghyun Kim <donghyun11.kim@lge.com>

--- src/third_party/blink/renderer/core/layout/layout_text.h
+++ src/third_party/blink/renderer/core/layout/layout_text.h
@@ -197,6 +197,7 @@ class CORE_EXPORT LayoutText : public LayoutObject {
                          unsigned offset,
                          unsigned len);
   void SetTextInternal(scoped_refptr<StringImpl>);
+  void UpdateTextIfNeeded();
 
   virtual void TransformText();
 
@@ -451,6 +452,7 @@ class CORE_EXPORT LayoutText : public LayoutObject {
   float last_line_line_min_width_;
 
   String text_;
+  String previous_text_;
 
   union {
     // The line boxes associated with this object.

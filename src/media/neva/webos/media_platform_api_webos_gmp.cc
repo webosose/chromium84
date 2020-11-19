d03d6840f1a09bda42039cbbc619be54e5aeb668
karthick.sundaram <karthick.sundaram@lge.com>
2020-11-19 11:11:48 +0000

    [op][n_upsable][media] Avoid pushing incorrect EOS to media pipeline
    :Release Notes:
    Fix pushing incorrect EOS to media pipeline to avoid video decode error
    
    :Detailed Notes:
    Whenever Video resolution is changed, EOS is received for Video and
    the same is recorded in MediaPlatformAPI. Now, if Audio EOS is received
    even before the end of Video buffers, MediaPlatformAPI pushes EOS packet
    to the media pipeline. After this, Video buffers are still received at
    MediaPlatformAPI and when these buffers are fed to media pipeline,
    pipeline throws media decode error. Added changes to reset
    video_eos_received_ flag, in case it was set by false EOS buffer.
    
    :Testing Performed:
    1. Launch "Shaka Player" built-in app
    2. Click ALL CONTENT menu at top side.
    3. From the combo box on the DRM text below at topside, Select the
       Widevine value to find Widevine videos.
    4. "Angel one" video should play without error.
    
    :QA Notes:
    Test-cases: GT-27193
    
    :Issues Addressed:
    [PLAT-123996] Angel one video is not played in SHAKA PLAYER app.
    
    Change-Id: Ifd32fe75af1731f62e04249657bb9e0b7152dd16
    Originally-Reviewed-on: http://gpro.lge.com/295628
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium84/+/296696
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Donghyun Kim <donghyun11.kim@lge.com>
    Tested-by: Donghyun Kim <donghyun11.kim@lge.com>

--- src/media/neva/webos/media_platform_api_webos_gmp.cc
+++ src/media/neva/webos/media_platform_api_webos_gmp.cc
@@ -679,6 +679,15 @@ MediaPlatformAPIWebOSGmp::FeedStatus MediaPlatformAPIWebOSGmp::FeedInternal(
     return kFeedSucceeded;
   }
 
+  // Whenever Video resolution is changed, EOS is received for Video. Now, if
+  // Actual Audio EOS is received even before the end of Video buffers,
+  // MediaPlatformAPI pushes EOS packet to the media pipeline. Reset the
+  // video_eos_received_ flag in case it was set by false EOS buffer.
+  if (video_eos_received_ && type == FeedType::kVideo &&
+      !buffer->end_of_stream()) {
+    video_eos_received_ = false;
+  }
+
   MEDIA_DATA_CHANNEL_T es_type = MEDIA_DATA_CH_NONE;
   if (type == FeedType::kVideo)
     es_type = MEDIA_DATA_CH_A;

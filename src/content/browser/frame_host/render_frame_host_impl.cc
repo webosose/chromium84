a2d1a078bb77857e363b5a378b8085c61bb99b10
Jose Dapena <jose.dapena@lge.com>
2020-12-02 16:36:18 +0300

    [op][n_upsable][file_access][whitelisting] Allow access to local file subresources from network origins
    :Release Notes:
    Register file scheme and URL loader also for remote origins in app runtime.
    It allow access to local file subresources from network origins.
    
    :Detailed Notes:
    As Neva app runtime provides fine-grained file access permission
    depending on the web app, using the file access delegate, we can
    allow general file access from remote origins. Then, file access
    delegate will be checked to decide if permission is allowed.
    
    To implement this, a new method is added to ContentBrowserClient
    IsFileAccessAllowedFromNetwork(). It is defined by default to false,
    but overriden to true in app runtime.
    
    :Testing Performed:
    1. Follow GT-27844
    2. For TC2 valid image should be displayed
    3. For TC4 video should be playing
    4. For TC6 hyperlink should open new document
    
    :QA Notes:
    Test-cases: GT-21986, GT-27844
    
    :Issues Addressed:
    [PLAT-100544] [chromium79]When switch Photos & Videos with LG Stores, LG store launches normally but it dose not resumes playing Ad
    [PLAT-101304] Valid image is not displayed for TC2 in the Netcast app.
    [PLAT-117039] Port [op][n_upsable][file_access][whitelisting] Allow access to local file subresources from network origins
    
    Change-Id: I4c6ba9a9a18b21780e1ebd354b3d41affd96bd73
    Originally-Reviewed-on: http://gpro.lge.com/c/webos-pro/chromium79/+/269742
    Reviewed-on: http://gpro.lge.com/c/webos-pro/chromium84/+/297779
    Reviewed-by: Commit Msg Checker <commit_msg@lge.com>
    Reviewed-by: Ban Word Checker <ban_word@lge.com>
    Reviewed-by: Yuri Sorochkin <yuri.sorochkin@lge.com>
    Reviewed-by: Sergey Kipet <sergey.kipet@lge.com>
    Reviewed-by: Dmitry Ketov <dmitry.ketov@lge.com>
    Reviewed-by: Jose Dapena <jose.dapena@lge.com>
    Reviewed-by: Vladislav Mukulov <vladislav.mukulov@lge.com>
    Reviewed-by: Donghyun Kim <donghyun11.kim@lge.com>
    Tested-by: Alexander Trofimov <alexander.trofimov@lge.com>
    Tested-by: Test BTS <test.bts@lge.com>
    Reviewed-on: http://gpro.lge.com/c/webosose/chromium84/+/298072
    Tested-by: Donghyun Kim <donghyun11.kim@lge.com>

--- src/content/browser/frame_host/render_frame_host_impl.cc
+++ src/content/browser/frame_host/render_frame_host_impl.cc
@@ -667,6 +667,14 @@ bool ParentNeedsTrustTokenFeaturePolicy(
   return false;
 }
 
+static bool IsFileAccessAllowedFromNetwork() {
+#if defined(USE_NEVA_APPRUNTIME)
+  return GetContentClient()->browser()->IsFileAccessAllowedFromNetwork();
+#else
+  return false;
+#endif
+}
+
 }  // namespace
 
 bool CreateNewHostForCrashedFrame() {
@@ -5826,6 +5834,7 @@ void RenderFrameHostImpl::CommitNavigation(
     // TODO(crbug.com/888079): In the future, use
     // GetOriginForURLLoaderFactory/GetOriginToCommit.
     if ((common_params->url.SchemeIsFile() ||
+         IsFileAccessAllowedFromNetwork() ||
          (common_params->url.IsAboutBlank() &&
           common_params->initiator_origin &&
           common_params->initiator_origin->scheme() == url::kFileScheme)) &&
